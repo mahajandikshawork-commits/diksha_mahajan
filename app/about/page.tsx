bd2ac1b1ed17536ba3f1e142a46edf6637c7301f
0xayushm <mangal.ayush.4982@gmail.com>
2026-01-11 21:22:42 +0530

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,7 +16,7 @@ export default function AboutPage() {
               </p>
             ))}
           </div>
-          <h1 className="text-sm md:text-base italic font-medium tracking-[0.2em] text-center md:mb-16 md:mt-8 mb-8 uppercase">
+          <h1 className="text-sm md:text-base italic font-medium tracking-[0.2em] text-center md:mb-16 mt-8 mb-8 uppercase">
             - Diksha Mahajan
           </h1>
         </div>
